22551f7c87e217e589d4cc8190721f0e0437e21b
Kristofer <kristofer@fmckl.se>
2024-07-02 14:24:36 +0200

    Add armitage_contributor recipe

---
+++ recipes/armitage_contributor/processor.js
@@ -0,0 +1,19 @@
+if (!queryResult[0].attestations[0]) {
+  throw new Error("Couldn't find any Armitage contributions for this address.");
+}
+
+const decodedDataJson = JSON.parse(
+  queryResult[0].attestations[0].decodedDataJson
+);
+
+const userCredScore = decodedDataJson.find(
+  (item) => item.name === "userCredScore"
+)?.value?.value;
+
+if (userCredScore < 100) {
+  throw new Error("A cred score of 100 is required for this attestation.");
+}
+
+return JSON.stringify([
+  { name: "armitage_contributor", type: "bool", value: true },
+]);
---
+++ recipes/armitage_contributor/recipe.js
@@ -0,0 +1,34 @@
+const recipe = {
+  name: "armitage_contributor",
+  queries: [
+    {
+      endpoint: "https://sepolia.easscan.org/graphql",
+      query: `
+        query ArmitageContributorQuery($where: AttestationWhereInput)
+        {
+          attestations(where: $where)
+          {
+            decodedDataJson
+          }
+        }      
+      `,
+      variables: {
+        where: {
+          schemaId: {
+            equals:
+              "0xdd55069b521b51585a2d09bed03cecde4524d4392683dc3c3c3cf09755a2a5b2",
+          },
+          attester: {
+            equals: "{user_eth_address}",
+            mode: "insensitive",
+          },
+        },
+      },
+    },
+  ],
+  schema: "bool armitage_contributor",
+  resolver: "0x0000000000000000000000000000000000000000",
+  revokable: false,
+};
+
+module.exports = recipe;
