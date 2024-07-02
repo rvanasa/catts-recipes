8710bbd224ce860c69cd7357ede8b5d3dafaec8c
Kristofer <kristofer@fmckl.se>
2024-07-02 14:16:57 +0200

    Add eu_gtc_passport_clone recipe

---
+++ recipes/gtc_passport_clone/processor.js
@@ -0,0 +1,18 @@
+if (!queryResult[0].attestations[0]) {
+  throw new Error("Couldn't find a Gitcoin Passport score for this address.");
+}
+
+const decodedDataJson = JSON.parse(
+  queryResult[0].attestations[0].decodedDataJson
+);
+
+let data = [];
+for (const item of decodedDataJson) {
+  data.push({
+    name: item.name,
+    type: item.type,
+    value: item.name === "score" ? item.value.value.hex : item.value.value,
+  });
+}
+
+return JSON.stringify(data);
