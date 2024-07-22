53cb32f20c2a7f052e3e7dd67c695c82d0032d47
Kristofer <kristofer@fmckl.se>
2024-07-22 15:17:38 +0200

    Processor changes

--- recipes/eu_gtc_passport_30/processor.js
+++ recipes/eu_gtc_passport_30/processor.js
@@ -28,20 +28,22 @@ const EU_COUNTRIES = [
   "SK",
 ];
 
-if (!queryResult[0].attestations[0]) {
+if (!queryResult[0].data.attestations[0]) {
   throw new Error("Couldn't find a Gitcoin Passport score for this address.");
 }
 
-if (!queryResult[1].attestations[0]) {
+if (!queryResult[1].data.attestations[0]) {
   throw new Error("Couldn't find country of residence for this address.");
 }
 
-const scoreHex = JSON.parse(queryResult[0].attestations[0].decodedDataJson)[0]
-  .value.value.hex;
+const scoreHex = JSON.parse(
+  queryResult[0].data.attestations[0].decodedDataJson
+)[0].value.value.hex;
 const score = BigInt(scoreHex);
 const requiredScore = BigInt("30000000000000000000");
-const country = JSON.parse(queryResult[1].attestations[0].decodedDataJson)[0]
-  .value.value;
+const country = JSON.parse(
+  queryResult[1].data.attestations[0].decodedDataJson
+)[0].value.value;
 const eligible = EU_COUNTRIES.includes(country) && score >= requiredScore;
 
 if (!eligible) throw new Error("Not eligible for eu_gtc_passport_30");
--- recipes/gtc_passport_clone/processor.js
+++ recipes/gtc_passport_clone/processor.js
@@ -1,9 +1,9 @@
-if (!queryResult[0].attestations[0]) {
+if (!queryResult[0].data.attestations[0]) {
   throw new Error("Couldn't find a Gitcoin Passport score for this address.");
 }
 
 const decodedDataJson = JSON.parse(
-  queryResult[0].attestations[0].decodedDataJson
+  queryResult[0].data.attestations[0].decodedDataJson
 );
 
 let data = [];
